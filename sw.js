d9aa79b6d792e9b43e437d1ccfe6f30646f1846a
iShahad97 <37577803+iShahad97@users.noreply.github.com>
2019-05-17 02:52:42 +0300

    Enter commit message here

--- sw.js
+++ sw.js
@@ -1,24 +1,27 @@
-var static_cache = 'my-site-cache-v1';
-var urlsToCache = [
-  './',
-  './index.html',
-  './restaurant.html',
-  './css/styles.css',
-  './js/main.js',
-  './js/restaurant_info.js',
-  './js/dbhelper.js',
-  './js/serviceWorker_reg.js',
-  './data/restaurants.json',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg',
+"use strict";
+
+const static_cache = 'my-site-cache-v1';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  'css/styles.css',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'js/serviceWorker_reg.js',
 ];
 
 self.addEventListener('install', function(event) {
@@ -34,15 +37,19 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request, {ignoreSearch: true}).then(function(response) {
+      if (response) {
+        return response;
       }
-    )
+      return fetch(event.request).then(function(response) {
+        return caches.open(static_cache).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(function(err) {
+        throw err;
+      });
+    })
   );
 });
 
